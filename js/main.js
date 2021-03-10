5aec879f186346fdf8881027e347767fbb1d706d
DenisPank <den3000d.1267@gmail.com>
2021-03-10 23:08:51 +0300

    slider

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new Swiper(".coffee__slider", {
+  // стрелки
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 2.5,
+  slidesPerColumn: 2,
+});
