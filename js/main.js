dca1e704cfeddcb4bf17941238285c95322d28a0
DenisPank <den3000d.1267@gmail.com>
2021-03-12 20:02:09 +0300

    work

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 new Swiper(".coffee__slider", {
-  // стрелки
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  slidesPerView: 2.5,
+  slidesPerView: 2.1,
   slidesPerColumn: 2,
+});
+
+var swiper = new Swiper(".giftset__slider", {
+  direction: "vertical",
+  spaceBetween: 60,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
+
+new Swiper(".combo__slider", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  slidesPerView: 3.2,
 });
