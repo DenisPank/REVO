e29a0946888f3e24ed7de8322e22e524ab62fec0
DenisPank <den3000d.1267@gmail.com>
2021-03-14 01:27:16 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -78,3 +78,26 @@ $(document).ready(function () {
     $("body").toggleClass("lock");
   });
 });
+
+$(document).ready(function () {
+  $(".header__link").click(function (event) {
+    $(".header__menu,.header__hamburger").removeClass("active");
+    $("body").removeClass("lock");
+  });
+});
+
+$(".scrollto").on("click", function () {
+  let href = $(this).attr("href");
+
+  $("html, body").animate(
+    {
+      scrollTop: $(href).offset().top,
+    },
+    {
+      duration: 600, // по умолчанию «400»
+      easing: "linear", // по умолчанию «swing»
+    }
+  );
+
+  return false;
+});
