e18d24454a1820210e2ddd1c01d9bdfffea006e4
DenisPank <den3000d.1267@gmail.com>
2021-03-13 22:45:28 +0300

    the layout is ready

--- js/main.js
+++ js/main.js
@@ -5,6 +5,28 @@ new Swiper(".coffee__slider", {
   },
   slidesPerView: 2.1,
   slidesPerColumn: 2,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    3000: {
+      slidesPerView: 3,
+    },
+    1400: {
+      slidesPerView: 2.1,
+    },
+
+    768: {
+      slidesPerColumn: 2,
+      slidesPerView: 1,
+    },
+
+    319: {
+      slidesPerView: 1,
+      slidesPerColumn: 1,
+    },
+  },
 });
 
 var swiper = new Swiper(".giftset__slider", {
@@ -24,7 +46,35 @@ new Swiper(".combo__slider", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   slidesPerView: 1,
   slidesPerView: 3.2,
   loop: true,
+  breakpoints: {
+    3000: {
+      slidesPerView: 5,
+    },
+
+    1400: {
+      slidesPerView: 3.2,
+    },
+
+    768: {
+      slidesPerView: 2,
+    },
+
+    319: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+$(document).ready(function () {
+  $(".header__hamburger").click(function (event) {
+    $(".header__hamburger, .header__menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
 });
